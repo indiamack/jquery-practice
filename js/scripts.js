69067d7e8b571915eba2f8fe50cffdae45b78ba5
India Mackinson <indjomac@gmail.com>
2018-10-03 12:29:04 -0400

    Opening repo. Class exercise on j-query

---
+++ js/scripts.js
@@ -0,0 +1,28 @@
+$(document).ready(function(){
+
+console.log('jQ loaded');
+
+var bands = ['Cage the Elephant', 'Mitski', 'Lana Del Rey'];
+var html = '';
+
+$('#box1').click(function(){
+  console.log('box 1 clicked');
+  $.each(bands, function(i, band){
+    html += '<li>' + bands[i] + '</li>';
+  });
+  $('#bands-list').html(html);
+  $('#modal1').fadeToggle();
+  $('#modal1').css('display', 'flex');
+});
+
+$('#box2').click(function(){
+  console.log('box 1 clicked');
+  $('#modal2').fadeToggle();
+  $('#modal2').css('display', 'flex');
+});
+
+$('.timeline').mouseenter(function(){
+  $(this).css('background', 'orange');
+});
+
+});
